613a0114aaae4de7e80ed643b982c0a41f4eca62
Bryan Long <BryanLong1@gmail.com>
2017-12-18 11:04:28 -0700

    backend boilerplate

---
+++ data.js
@@ -0,0 +1,20 @@
+let data = [
+  {
+    "id": 1,
+    "data": "Data Piece 1"
+  },
+  {
+    "id": 2,
+    "data": "Data Piece 2"
+  },
+  {
+    "id": 3,
+    "data": "Data Piece 3"
+  },
+]
+
+
+
+module.exports = {
+  data
+}
