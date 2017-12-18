6ee60d2d45f98f075826f182efd10694313919b0
Bryan Long <BryanLong1@gmail.com>
2017-12-18 15:41:53 -0700

    14ers Initial Backend Structure

---
+++ class.js
@@ -0,0 +1,471 @@
+let class = [{
+  "id": 1,
+  "attributes": {
+    "peak_name": "Mt. Elbert",
+    "class": 1,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 10
+  }
+}, {
+  "id": 2,
+  "attributes": {
+    "peak_name": "Mt. Massive",
+    "class": 2,
+    "standard_route": "East Slopes",
+    "roundtrip_distance": 15
+  }
+}, {
+  "id": 3,
+  "attributes": {
+    "peak_name": "Mt. Harvard",
+    "class": 2,
+    "standard_route": "South Slopes",
+    "roundtrip_distance": 14
+  }
+}, {
+  "id": 4,
+  "attributes": {
+    "peak_name": "Blanca Peak",
+    "class": 2,
+    "standard_route": "Northwest Ridge",
+    "roundtrip_distance": 17
+  }
+}, {
+  "id": 5,
+  "attributes": {
+    "peak_name": "La Plata Peak",
+    "class": 2,
+    "standard_route": "Northwest Ridge",
+    "roundtrip_distance": 9
+  }
+}, {
+  "id": 6,
+  "attributes": {
+    "peak_name": "Uncompahgre Peak",
+    "class": 2,
+    "standard_route": "South Ridge",
+    "roundtrip_distance": 8
+  }
+}, {
+  "id": 7,
+  "attributes": {
+    "peak_name": "Crestone Peak",
+    "class": 3,
+    "standard_route": "South Face",
+    "roundtrip_distance": 14
+  }
+}, {
+  "id": 8,
+  "attributes": {
+    "peak_name": "Mt. Lincoln",
+    "class": 2,
+    "standard_route": "West Ridge",
+    "roundtrip_distance": 6
+  }
+}, {
+  "id": 9,
+  "attributes": {
+    "peak_name": "Grays Peak",
+    "class": 1,
+    "standard_route": "East Slopes",
+    "roundtrip_distance": 8
+  }
+}, {
+  "id": 10,
+  "attributes": {
+    "peak_name": "Mt. Antero",
+    "class": 2,
+    "standard_route": "West Slopes",
+    "roundtrip_distance": 16
+  }
+}, {
+  "id": 11,
+  "attributes": {
+    "peak_name": "Torreys Peak",
+    "class": 2,
+    "standard_route": "South Slopes",
+    "roundtrip_distance": 8
+  }
+}, {
+  "id": 12,
+  "attributes": {
+    "peak_name": "Castle Peak",
+    "class": 2,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 14
+  }
+}, {
+  "id": 13,
+  "attributes": {
+    "peak_name": "Quandary Peak",
+    "class": 1,
+    "standard_route": "East Ridge",
+    "roundtrip_distance": 7
+  }
+}, {
+  "id": 14,
+  "attributes": {
+    "peak_name": "Mt. Evans",
+    "class": 2,
+    "standard_route": "West Ridge",
+    "roundtrip_distance": 5
+  }
+}, {
+  "id": 15,
+  "attributes": {
+    "peak_name": "Longs Peak",
+    "class": 3,
+    "standard_route": "Keyhole Route",
+    "roundtrip_distance": 15
+  }
+}, {
+  "id": 16,
+  "attributes": {
+    "peak_name": "Mt. Wilson",
+    "class": 4,
+    "standard_route": "North Slopes",
+    "roundtrip_distance": 16
+  }
+}, {
+  "id": 17,
+  "attributes": {
+    "peak_name": "Mt. Cameron",
+    "class": 2,
+    "standard_route": "Combo: Democrat, Cameron, Lincoln, Bross",
+    "roundtrip_distance": 7
+  }
+}, {
+  "id": 18,
+  "attributes": {
+    "peak_name": "Mt. Shavano",
+    "class": 9,
+    "standard_route": "East Slopes",
+    "roundtrip_distance": 9
+  }
+}, {
+  "id": 19,
+  "attributes": {
+    "peak_name": "Mt. Belford",
+    "class": 2,
+    "standard_route": "Northwest Ridge",
+    "roundtrip_distance": 8
+  }
+}, {
+  "id": 20,
+  "attributes": {
+    "peak_name": "Crestone Needle",
+    "class": 3,
+    "standard_route": "South Face",
+    "roundtrip_distance": 12
+  }
+}, {
+  "id": 21,
+  "attributes": {
+    "peak_name": "Mt. Princeton",
+    "class": 2,
+    "standard_route": "East Slopes",
+    "roundtrip_distance": 6
+  }
+}, {
+  "id": 22,
+  "attributes": {
+    "peak_name": "Mt. Yale",
+    "class": 2,
+    "standard_route": "Southwest Slopes",
+    "roundtrip_distance": 9
+  }
+}, {
+  "id": 23,
+  "attributes": {
+    "peak_name": "Mt. Bross",
+    "class": 2,
+    "standard_route": "Combo: Democrat, Cameron, Lincoln, Bross",
+    "roundtrip_distance": 7
+  }
+}, {
+  "id": 24,
+  "attributes": {
+    "peak_name": "Kit Carson Peak",
+    "class": 3,
+    "standard_route": "From Challenger Point",
+    "roundtrip_distance": 14
+    }
+}, {
+  "id": 25,
+  "attributes": {
+    "peak_name": "El Diente Peak",
+    "class": 3,
+    "standard_route": "North Slopes",
+    "roundtrip_distance": 15
+  }
+}, {
+  "id": 26,
+  "attributes": {
+    "peak_name": "Maroon Peak",
+    "class": 3,
+    "standard_route": "South Ridge",
+    "roundtrip_distance": 11
+    }
+}, {
+  "id": 27,
+  "attributes": {
+    "peak_name": "Tabeguache Peak",
+    "class": 2,
+    "standard_route": "Via Shavano",
+    "roundtrip_distance": 11
+    }
+}, {
+  "id": 28,
+  "attributes": {
+    "peak_name": "Mt. Oxford",
+    "class": 2,
+    "standard_route": "Via Mt. Belford",
+    "roundtrip_distance": 11
+    }
+}, {
+  "id": 29,
+  "attributes": {
+    "peak_name": "Mt. Sneffels",
+    "class": 3,
+    "standard_route": "From Yankee Boy Basin",
+    "roundtrip_distance": 6
+    }
+}, {
+  "id": 30,
+  "attributes": {
+    "peak_name": "Mt. Democrat",
+    "class": 2,
+    "standard_route": "Combo: Democrat, Cameron, Lincoln, Bross",
+    "roundtrip_distance": 7
+    }
+}, {
+  "id": 31,
+  "attributes": {
+    "peak_name": "Capitol Peak",
+    "class": 4,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 17
+    }
+}, {
+  "id": 32,
+  "attributes": {
+    "peak_name": "Pikes Peak",
+    "class": 2,
+    "standard_route": "Northwest Slopes",
+    "roundtrip_distance": 14
+    }
+  }, {
+  "id": 33,
+  "attributes": {
+    "peak_name": "Snowmass Mountain",
+    "class": 3,
+    "standard_route": "East Slopes",
+    "roundtrip_distance": 22
+    }
+}, {
+  "id": 34,
+  "attributes": {
+    "peak_name": "Mt. Eolus",
+    "class": 3,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 5
+    }
+  }, {
+  "id": 35,
+  "attributes": {
+    "peak_name": "Windom Peak",
+    "class": 2,
+    "standard_route": "West Ridge",
+    "roundtrip_distance": 5
+    }
+}, {
+  "id": 36,
+  "attributes": {
+    "peak_name": "Challenger Point",
+    "class": 2,
+    "standard_route": "North Slopes",
+    "roundtrip_distance": 12
+    }
+}, {
+  "id": 37,
+  "attributes": {
+    "peak_name": "Mt. Columbia",
+    "class": 2,
+    "standard_route": "West Slopes",
+    "roundtrip_distance": 11
+    }
+}, {
+  "id": 38,
+  "attributes": {
+    "peak_name": "Missouri Mountain",
+    "class": 2,
+    "standard_route": "Northwest Ridge",
+    "roundtrip_distance": 10
+  }
+}, {
+  "id": 39,
+  "attributes": {
+    "peak_name": "Humboldt Peak",
+    "class": 2,
+    "standard_route": "West Ridge",
+    "roundtrip_distance": 11
+    }
+}, {
+  "id": 40,
+  "attributes": {
+    "peak_name": "Mt. Bierstadt",
+    "class": 2,
+    "standard_route": "West Slopes",
+    "roundtrip_distance": 7
+  }
+}, {
+  "id": 41,
+  "attributes": {
+    "peak_name": "Conundrum Peak",
+    "class": 2,
+    "standard_route": "Via Castle",
+    "roundtrip_distance": 14
+    }
+}, {
+  "id": 42,
+  "attributes": {
+    "peak_name": "Sunlight Peak",
+    "class": 4,
+    "standard_route": "South Face",
+    "roundtrip_distance": 5
+    }
+}, {
+  "id": 43,
+  "attributes": {
+    "peak_name": "Handie Peak",
+    "class": 1,
+    "standard_route": "Southwest Slopes",
+    "roundtrip_distance": 6
+    }
+  }, {
+  "id": 44,
+  "attributes": {
+    "peak_name": "Culebra Peak",
+    "class": 2,
+    "standard_route": "Northwest Ridge",
+    "roundtrip_distance": 5
+    }
+  }, {
+  "id": 45,
+  "attributes": {
+    "peak_name": "Ellingwood Point",
+    "class": 2,
+    "standard_route": "South Face",
+    "roundtrip_distance": 17
+  }
+}, {
+  "id": 46,
+  "attributes": {
+    "peak_name": "Mt. Lindsey",
+    "class": 3,
+    "standard_route": "North Face",
+    "roundtrip_distance": 8
+    }
+  }, {
+  "id": 47,
+  "attributes": {
+    "peak_name": "North Eolus",
+    "class": 3,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 5
+  }
+}, {
+  "id": 48,
+  "attributes": {
+    "peak_name": "Little Bear Peak",
+    "class": 4,
+    "standard_route": "West Ridge",
+    "roundtrip_distance": 14
+    }
+  }, {
+  "id": 49,
+  "attributes": {
+    "peak_name": "Mt. Sherman",
+    "class": 2,
+    "standard_route": "Southwest Ridge",
+    "roundtrip_distance": 5
+  }
+}, {
+  "id": 50,
+  "attributes": {
+    "peak_name": "Redcloud Peak",
+    "class": 2,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 5
+  }
+}, {
+  "id": 51,
+  "attributes": {
+    "peak_name": "Pyramid Peak",
+    "class": 4,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 8
+  }
+}, {
+  "id": 52,
+  "attributes": {
+    "peak_name": "Wilson Peak",
+    "class": 4,
+    "standard_route": "North Slopes",
+    "roundtrip_distance": 16
+  }
+}, {
+  "id": 53,
+  "attributes": {
+    "peak_name": "Wetterhorn Peak",
+    "class": 3,
+    "standard_route": "Southwest Ridge",
+    "roundtrip_distance": 7
+  }
+}, {
+  "id": 54,
+  "attributes": {
+    "peak_name": "San Luis Peak",
+    "class": 1,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 14
+  }
+}, {
+  "id": 55,
+  "attributes": {
+    "peak_name": "North Maroon Peak",
+    "class": 4,
+    "standard_route": "Northeast Ridge",
+    "roundtrip_distance": 8
+  }
+}, {
+  "id": 56,
+  "attributes": {
+    "peak_name": "Mt. of the Holy Cross",
+    "class": 12
+    "standard_route": "North Ridge",
+    "roundtrip_distance": 12
+  }
+}, {
+  "id": 57,
+  "attributes": {
+    "peak_name": "Huron Peak",
+    "class": 2,
+    "standard_route": "Northwest Slopes",
+    "roundtrip_distance": 6
+  }
+}, {
+  "id": 58,
+  "attributes": {
+    "peak_name": "Sunshine Peak",
+    "class": 2,
+    "standard_route": "From Redcloud",
+    "roundtrip_distance": 12
+  }
+}]
+
+
+
+module.exports = {
+  class
+}
