6bb3579e067f51ca84480a81c8f91c21c95e73ab
Bryan Long <BryanLong1@gmail.com>
2017-12-18 11:16:54 -0700

    Data

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const data = require("./data.js");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Big World!');
 });
 
+console.log(data);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Example app listening on port 3000!');
